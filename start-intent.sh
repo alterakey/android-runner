69cba958de11cbc19d1a1c788752f6d6d3e5a744
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-08-01 10:35:00 +0900

    Using more general dialect

--- start-intent.sh
+++ start-intent.sh
@@ -1,14 +1,14 @@
-#/bin/sh
+#/bin/bash
 intent=$1
 pkg=$2
 activity=$3
 
-function intr() {
+intr() {
   adb shell am start -a android.intent.action.MAIN -c android.intent.category.HOME
   exit 0
 }
 
-trap intr SIGINT
+trap intr INT
 
 adb shell am start -a $intent -n $pkg/$activity &
 adb logcat
