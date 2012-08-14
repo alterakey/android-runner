2273f8b3e6fb3b70972c2ba89e2bc3be710866e5
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-08-14 23:08:33 +0900

    Using SSA by default

--- test-unit-TestRunner.template.java
+++ test-unit-TestRunner.template.java
@@ -1,10 +1,18 @@
 package x.y.z;
 
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowSimpleAdapter;
 import org.junit.runners.model.InitializationError;
 
 public class TestRunner extends RobolectricTestRunner {
     public TestRunner(Class testClass) throws InitializationError {
         super(testClass);
+    }
+
+    @Override
+    protected void bindShadowClasses() {
+        super.bindShadowClasses();
+        Robolectric.bindShadowClass(ShadowSimpleAdapter.class);
     }
 }
